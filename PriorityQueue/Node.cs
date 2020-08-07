5138a330e4fa3d126da9839e3734e6ade722550e
Srujana-Jami <srujana.jami@innroad.com>
2020-08-07 13:28:04 +0530

    Add project files.

---
+++ PriorityQueue/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriorityQueue
+{
+    class Node
+    {
+        public int priority;
+        public int info;
+        public Node link;
+
+        public Node(int i ,int pr)
+        {
+            info = i;
+            priority = pr;
+            link = null;
+        }
+    
+    
+    }
+}
